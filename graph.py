e0c0153ada5a9d590c66c0b978bea7565dbde682
Joseph <119084558+DerjenigeUberMensch@users.noreply.github.com>
2023-04-03 15:42:11 -0700

    var changes  (NOTimportant)
    Added a variable so you can change what file you are refering to easier

--- graph.py
+++ graph.py
@@ -3,8 +3,9 @@ import pandas as pd
 import time
 import os
 def main():
+    file_name = "ETHN.txt"
     try:
-        with open('ETH.txt', 'r') as file:
+        with open(file_name, 'r') as file:
             var = [i for i in file]
     except FileNotFoundError:
         return None
@@ -14,9 +15,10 @@ def main():
     plt.xlabel("days")
     plt.title("Stock Market Prices over time")
     plt.plot(x,y, c="black",linewidth=2)
+    plt.tight_layout() # fit to window
     plt.show()
     plt.close()
-    with open('nums.txt', 'w') as f:
+    with open(file_name, 'w') as f:
         f.truncate()
         f.close()
 
