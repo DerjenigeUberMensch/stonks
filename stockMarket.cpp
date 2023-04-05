78b7534ea7f9296c81a1e0f913f3d5ce86401e53
Joseph <119084558+DerjenigeUberMensch@users.noreply.github.com>
2023-04-05 16:38:57 -0700

    Changed Display System
    Made it somewhat future proof to use for loop iterate over each stock and print to console

--- stockMarket.cpp
+++ stockMarket.cpp
@@ -9,7 +9,7 @@
 #include <vector>
 
 class Stock {
-public:
+public:    
   std::string symbol;
   std::string stock_name;
   float volatility;
@@ -28,7 +28,6 @@ protected:
   std::string bitBurnerSym = "w0r1d_d43m0n";
   std::string stockName = "BitBurner";
 };
-
 void update_stock_price(Stock &stock){
   //generate new stock price
   int rise_or_dip = rng(-1, 1);
@@ -61,118 +60,132 @@ void new_stock_price(Stock &stock, const int numOfIterations = 1) {
   }
 
 }
+std::string display_stock_price(const Stock stock, int cout_spaces = 10){
+  int stock_length = stock.stock_name.length();
+  cout_spaces = cout_spaces - stock_length;
+  if(cout_spaces <= 0 ){
+    return " ";
+  }
+  std::string spaces(cout_spaces, ' ');
+  std::string display = stock.stock_name+':'+spaces+std::to_string(stock.current_price);
+  return display;
+}
 int main() {
+  std::vector<Stock> stocks;
   Stock NVIDIA;
   NVIDIA.volatility = 0.07;
   NVIDIA.symbol = "NVDA";
   NVIDIA.stock_name = "NVIDIA";
-  
+  stocks.push_back(NVIDIA);
 
   Stock Microsoft;
   Microsoft.volatility = .03;
   Microsoft.symbol = "MCSFT";
   Microsoft.stock_name = "Microsoft";
-  
+  stocks.push_back(Microsoft);
 
   Stock Apple;
   Apple.volatility = .027;
   Apple.symbol = "APP";
   Apple.stock_name = "Apple";
-  
+  stocks.push_back(Apple);
 
   Stock Oracle;
   Oracle.volatility = 0.06;
   Oracle.symbol = "ORC";
   Oracle.stock_name = "Oracle";
-  
+  stocks.push_back(Oracle);
 
   Stock Tesla;
   Tesla.volatility = .16;
   Tesla.symbol = "TSL";
   Tesla.stock_name = "Tesla";
-  
+  stocks.push_back(Tesla);
 
   Stock Ford;
   Ford.volatility = .039;
   Ford.symbol = "FORD";
   Ford.stock_name = "Ford";
-  
+  stocks.push_back(Ford);
 
   Stock Intel;
   Intel.volatility = .017;
   Intel.symbol = "INT";
   Intel.stock_name = "Intel";
-  
+  stocks.push_back(Intel);
 
   Stock Google;
   Google.volatility = .05;
   Google.symbol = "ABC";
   Google.stock_name = "Google";
-  
+  stocks.push_back(Google);
 
   Stock Fazbear;
   Fazbear.volatility = .03;
   Fazbear.symbol = "FAZ";
   Fazbear.stock_name = "Freddy Fazbears";
-  
+  stocks.push_back(Fazbear);
 
   Stock Coca_cola;
   Coca_cola.volatility = .04;
   Coca_cola.symbol = "COLA";
   Coca_cola.stock_name = "Coca Cola Co";
-  
+  stocks.push_back(Coca_cola);
 
   Stock Pepsi_co;
   Pepsi_co.volatility = .04;
   Pepsi_co.symbol = "PEP";
   Pepsi_co.stock_name = "Pepsi Co";
-  
+  stocks.push_back(Pepsi_co);
 
   Stock S_and_P;
   S_and_P.volatility = .05;
   S_and_P.symbol = "S&P";
   S_and_P.stock_name = "S&P500";
-  
+  stocks.push_back(S_and_P);
 
   Stock Dow_jones;
   Dow_jones.volatility = .07;
   Dow_jones.symbol = "DWJ";
   Dow_jones.stock_name = "Dow Jones";
-  
+  stocks.push_back(Dow_jones);
 
   Stock Oil;
   Oil.volatility = .10;
   Oil.symbol = "OIL";
   Oil.stock_name = "Oil";
-  
+  stocks.push_back(Oil);
 
   Stock Gold;
   Gold.volatility = .15;
   Gold.symbol = "GOLD";
   Gold.stock_name = "Gold";
-  
+  stocks.push_back(Gold);
 
   Stock Uranium;
   Uranium.volatility = .17;
   Uranium.symbol = "URM";
   Uranium.stock_name = "Uranium";
+  stocks.push_back(Uranium);
   
-  
-  Stock Natural_Gas;
-  Natural_Gas.volatility = .15;
-  Natural_Gas.symbol = "GAS";
-  Natural_Gas.stock_name = "Natural Gas";
-  
+  Stock Nat_gas; // Natural Gas
+  Nat_gas.volatility = .15;
+  Nat_gas.symbol = "GAS";
+  Nat_gas.stock_name = "Natural Gas";
+  stocks.push_back(Nat_gas);
 
   Stock Ethanol;
   Ethanol.volatility = .20;
   Ethanol.symbol = "ETHN";
   Ethanol.stock_name = "Ethanol";
-  
-  new_stock_price(Ethanol);
+  stocks.push_back(Ethanol);
 
-  int some_varaible;
   std::cout << "StockMarket" << "\n";
+  for(int i = 0;i < stocks.size();++i){
+    std::cout << display_stock_price(stocks[i], 20) << "\n";
+  }
+  
+  /*
   std::cout << NVIDIA.stock_name       << ": " << NVIDIA.current_price   << "\n";
   std::cout << Google.stock_name       << ": " << Google.current_price   << "\n";
   std::cout << Oracle.stock_name       << ": " << Oracle.current_price   << "\n";
@@ -185,13 +198,11 @@ int main() {
   std::cout << Pepsi_co.stock_name     << ": " << Pepsi_co.current_price << "\n";
   std::cout << Dow_jones.stock_name    << ": " << Dow_jones.current_price<< "\n";
   std::cout << S_and_P.stock_name      << ": " << S_and_P.current_price  << "\n";
-  std::cout << Natural_Gas.stock_name  << ": " << Natural_Gas.current_price<< "\n";
+  std::cout << Nat_gas.stock_name      << ": " << Nat_gas.current_price  << "\n";
   std::cout << Uranium.stock_name      << ": " << Uranium.current_price  << "\n";
   std::cout << Oil.stock_name          << ": " << Oil.current_price      << "\n";  
   std::cout << Ethanol.stock_name      << ": " << Ethanol.current_price  << "\n";
-
-
-  std::cin >> some_varaible;
+  */
   //writeToFile("stocks.txt",)
   return 0;
 }
