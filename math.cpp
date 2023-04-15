03f9b557ab4c06edb49ebcee740d2aeca5a90c38
Joseph <119084558+DerjenigeUberMensch@users.noreply.github.com>
2023-04-14 17:48:14 -0700

    Added isnumber() and is integer()

--- math.cpp
+++ math.cpp
@@ -5,6 +5,21 @@ int rng(int min, int max) {
     std::uniform_int_distribution<int> dist(min,max);
     return dist(rd);
 }
+inline bool isInteger(const std::string & s)
+{
+   if(s.empty() || ((!isdigit(s[0])) && (s[0] != '-') && (s[0] != '+'))) return false;
+
+   char * p;
+   strtol(s.c_str(), &p, 10);
+
+   return (*p == 0);
+}
+bool isNumber(std::string number){
+    for(int i = 0;i < number.length();++i){
+        if(isdigit(number[i]) != true && number[i] != '.'){return false;}
+    }
+    return true;
+}
 /*
 #include <random>
 int rng(int min, int max) {
